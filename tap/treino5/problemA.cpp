cf823526fe27c4b4be21c39dde12417053014ca0
João Vitor <109254917+pimenta9@users.noreply.github.com>
2024-05-10 22:52:41 -0300

    Create problemA.cpp

---
+++ tap/treino5/problemA.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main ()
+{
+    int n;
+    cin >> n;
+
+    map<int, int> m;
+    for (int i = 0; i < n; i++)
+    {
+        int a;
+        cin >> a;
+
+        if (m[a])
+        {
+            cout << "NO\n";
+            return 0;
+        }
+
+        m[a] = 1;
+    }
+
+    cout << "YES\n";
+
+    return 0;
+}
