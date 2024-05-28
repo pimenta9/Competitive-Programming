a29a247d26e141b92bacdab6cf312ff5bcba8c68
joao vitor <joaovfp2002@gmail.com>
2024-05-28 17:52:47 -0300

    LEET 1208

--- leet/leet1208.cpp
+++ leet/leet1208.cpp
@@ -1,3 +1,5 @@
+// ACCEPTED!
+
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -5,19 +7,39 @@ int equalSubstring(string s, string t, int maxCost)
 {
     int n = s.size();
 
-    int arr[n];
+    int arr[n + 1];
 
-    for (int i = 0; i < n; i++)
+    arr[0] = 0;
+    for (int i = 1; i <= n; i++)
     {
-        arr[i] = abs(s[i] - t[i]);
+        arr[i] = abs(s[i - 1] - t[i - 1]) + arr[i - 1];
     }
 
-    
+    int l = 0, r = 1, maxLength = 0, length, count;
+    while (r <= n)
+    {
+        count = arr[r] - arr[l];
+
+        if (count <= maxCost)
+        {
+            length = r - l;
+
+            if (length > maxLength) maxLength = length;
+
+            r++;
+        }
+        else
+        {
+            l++;
+        }
+    }
+
+    return maxLength;
 }
 
 int main ()
 {
-    string s1 = {"abcd"}, s2 = {"bcdf"};
+    string s1 = {"abcde"}, s2 = {"beeei"};
 
     cout << equalSubstring(s1, s2, 3) << endl;
 
