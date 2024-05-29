89c4c8568a63707a32ec9ec0b248971957d87ab1
joao vitor <joaovfp2002@gmail.com>
2024-05-29 00:15:27 -0300

    creating leet1404

---
+++ leet/leet1404.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int numSteps(string s)
+{
+    long long x = 0;
+    int n = s.size();
+
+    for (int i = (n - 1); i >= 0; i--)
+    {
+        x += 
+    }
+}
+
+int main ()
+{
+    string s = {"1101"};
+
+    cout << numSteps(s) << endl;
+
+    return 0;
+}
