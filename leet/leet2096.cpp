6de4546c7de316ce89fbfd4b02093fb36046e1e8
João Vitor <109254917+pimenta9@users.noreply.github.com>
2024-07-16 00:15:47 -0300

    Create leet2096.cpp

---
+++ leet/leet2096.cpp
@@ -0,0 +1,26 @@
+// STILL TRYING...
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+string getDirections(TreeNode* root, int startValue, int destValue)
+{
+
+}
+
+int main ()
+{
+
+
+    return 0;
+}
