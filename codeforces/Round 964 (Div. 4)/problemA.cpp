a42e663ad61fdffe2430bd6169767c54e13a7a0e
João Vitor <109254917+pimenta9@users.noreply.github.com>
2024-08-08 15:46:35 -0300

    Create problemA.cpp

---
+++ codeforces/Round 964 (Div. 4)/problemA.cpp
@@ -0,0 +1,18 @@
+// ACCEPTED!
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main ()
+{
+    int t; cin >> t;
+
+    while (t--)
+    {
+        string x; cin >> x;
+
+        cout << x[0] - '0' + x[1] - '0' << endl;
+    }
+
+    return 0;
+}
