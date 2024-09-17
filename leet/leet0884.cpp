6758cf1a9bb493b205fad0a0775d2ae307bfce0c
João Vitor <109254917+pimenta9@users.noreply.github.com>
2024-09-17 10:32:43 -0300

    daily September 17th 2024 (medium)

---
+++ leet/leet0884.cpp
@@ -0,0 +1,51 @@
+// ACCEPTED!
+
+class Solution {
+public:
+    vector<string> splitWords(string s)
+    {
+        vector<string> words;
+
+        int i, j;
+        for(i = 0; i < s.size(); i = j + 1)
+        {
+            string str;
+            for(j = i; s[j] != ' ' and s[j] != '\0'; j++)
+            {
+                str.push_back(s[j]);
+            }
+
+            words.push_back(str);
+        }
+
+        return words;
+    }
+
+    vector<string> uncommonFromSentences(string s1, string s2)
+    {
+        map<string, int> wordFreq;
+
+        vector<string> v1 = splitWords(s1);
+        for(string s : v1)
+            wordFreq[s]++;
+
+        vector<string> v2 = splitWords(s2);
+        for(string s : v2)
+            wordFreq[s]++;
+
+        vector<string> uncommonWords;
+
+        while(!wordFreq.empty())
+        {
+            string s = (*wordFreq.begin()).first;
+            int freq = (*wordFreq.begin()).second;
+
+            wordFreq.erase(s);
+
+            if(freq == 1)
+                uncommonWords.push_back(s);
+        }
+
+        return uncommonWords;
+    }
+};
