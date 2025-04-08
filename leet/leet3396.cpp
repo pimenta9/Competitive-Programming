99ba1de390b3dd0983849f9cae1770fda74a43f7
João Vitor <109254917+pimenta9@users.noreply.github.com>
2025-04-07 23:59:03 -0300

    easy, beats 100%

---
+++ leet/leet3396.cpp
@@ -0,0 +1,18 @@
+// ACCEPTED!
+
+class Solution {
+public:
+    int minimumOperations(vector<int>& nums)
+    {
+        vector<bool> seen(100, false);
+        for (int i = nums.size()-1; i >= 0; i--)
+        {
+            if (seen[nums[i]])
+                return i/3 + 1;
+
+            seen[nums[i]] = true;
+        }
+
+        return 0;
+    }
+};
