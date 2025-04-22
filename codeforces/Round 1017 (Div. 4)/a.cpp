85cb93b4d0a4339a61ce142befe9156579e88b91
João Vitor <109254917+pimenta9@users.noreply.github.com>
2025-04-22 19:39:23 -0300

    ACCEPTED!

---
+++ codeforces/Round 1017 (Div. 4)/a.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main ()
+{
+    int t;
+    cin >> t;
+ 
+    while (t--)
+    {
+        char s1[11];
+        char s2[11];
+        char s3[11];
+ 
+        scanf("%s", s1);
+        scanf("%s", s2);
+        scanf("%s", s3);
+ 
+        cout << s1[0] << s2[0] << s3[0] << endl;
+    }
+ 
+    return 0;
+}
