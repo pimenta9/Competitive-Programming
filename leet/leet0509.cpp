5790899b80dc75b730a370c4996076978318aff9
João Vitor <109254917+pimenta9@users.noreply.github.com>
2025-04-30 19:12:28 -0300

    easy, beats 76.24%

---
+++ leet/leet0509.cpp
@@ -0,0 +1,16 @@
+// ACCEPTED!
+
+class Solution {
+public:
+    int fib(int n)
+    {
+        int dp[max(n+1, 2)];
+        dp[0] = 0;
+        dp[1] = 1;
+
+        for (int i = 2; i <= n; i++)
+            dp[i] = dp[i-1] + dp[i-2];
+
+        return dp[n];
+    }
+};
