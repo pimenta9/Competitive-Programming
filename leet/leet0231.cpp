cbc397d980e331b201b03c762b4d5572cd961399
João Vitor <109254917+pimenta9@users.noreply.github.com>
2025-08-08 21:09:03 -0300

    daily August 9th, 2025 (easy, beats 100%)

---
+++ leet/leet0231.cpp
@@ -0,0 +1,20 @@
+// ACCEPTED!
+
+class Solution {
+public:
+    bool isPowerOfTwo(int n)
+    {
+        if (n <= 0)
+            return false;
+
+        while (n > 1)
+        {
+            if (n & 1)
+                return false;
+
+            n = n >> 1;
+        }
+
+        return true;
+    }
+};
