57b8e7fd1eece45febaa719abdfcee34854c5e8e
João Vitor <109254917+pimenta9@users.noreply.github.com>
2025-08-12 21:14:27 -0300

    daily August 13th, 2025 (easy)

---
+++ leet/leet0326.cpp
@@ -0,0 +1,21 @@
+// ACCEPTED!
+
+class Solution {
+public:
+    bool isPowerOfThree(int n)
+    {
+        if (n < 0)
+            return false;
+
+        long long x = 1;
+        while (x <= n)
+        {
+            if (x == n)
+                return true;
+            
+            x *= 3;
+        }
+
+        return false;
+    }
+};
