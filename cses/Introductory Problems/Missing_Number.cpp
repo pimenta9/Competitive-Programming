c69e2791eed3d5df5120b4dcc68b071838c0ee62
João Vitor <109254917+pimenta9@users.noreply.github.com>
2025-10-06 14:51:57 -0300

    Create Missing_Number.cpp

---
+++ cses/Introductory Problems/Missing_Number.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main ()
+{
+    long long n; cin >> n;
+ 
+    long long sum = 0;
+ 
+    for (int i = 1; i <= n-1; i++)
+    {
+        int x; cin >> x;
+ 
+        sum += x;
+    }
+ 
+    cout << ((n*(n+1))/2 - sum) << '\n';
+ 
+    return 0;
+}
