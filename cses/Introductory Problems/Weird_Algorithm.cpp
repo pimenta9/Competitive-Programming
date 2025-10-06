3cefa0804f0c17e9c7be15474e4d4dd8abf4b6be
João Vitor <109254917+pimenta9@users.noreply.github.com>
2025-10-06 14:51:23 -0300

    Create Weird_Algorithm.cpp

---
+++ cses/Introductory Problems/Weird_Algorithm.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main ()
+{
+    unsigned long long n;
+    cin >> n;
+ 
+    while (n > 1)
+    {
+        cout << n << ' ';
+        if (n & 1)
+            n = n*3 + 1;
+        else
+            n = n/2;
+    }
+    cout << 1 << endl;
+ 
+    return 0;
+}
