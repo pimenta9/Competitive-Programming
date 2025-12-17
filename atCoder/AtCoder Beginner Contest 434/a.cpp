3dcfc36dbd8771967ea28675747149ffe7339b92
João Vitor <109254917+pimenta9@users.noreply.github.com>
2025-12-17 02:20:58 -0300

    Create a.cpp

---
+++ atCoder/AtCoder Beginner Contest 434/a.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+
+int32_t main ()
+{
+    cin.tie(0)->sync_with_stdio(0);
+
+    int w, b;
+    cin >> w >> b;
+    
+    cout << (1000*w)/b + 1 << '\n';
+
+    return 0;
+}
