0635bcb18b8e17ecae886718ae401af1b51dbd7b
João Vitor <109254917+pimenta9@users.noreply.github.com>
2026-02-09 14:47:03 -0300

    Add a.cpp for AtCoder Beginner Contest 443

---
+++ atCoder/AtCoder Beginner Contest 443/a.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+
+int32_t main ()
+{
+    cin.tie(0)->sync_with_stdio(0);
+
+    string s; cin >> s;
+    cout << s << "s\n";
+
+    return 0;
+}
