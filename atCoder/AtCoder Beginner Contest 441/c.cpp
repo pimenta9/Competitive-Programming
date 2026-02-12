1dbfeeae2a862fba52f8f542d5aa4bf90a91ebdc
João Vitor <109254917+pimenta9@users.noreply.github.com>
2026-02-12 16:02:50 -0300

    Create c.cpp

---
+++ atCoder/AtCoder Beginner Contest 441/c.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// defines
+#define pii pair<int, int>
+#define int long long
+#define ceil(n, m) (((n) + (m) - 1) / (m))
+#define add_mod(a, b, m) (((a % m) + (b % m)) % m)
+#define sub_mod(a, b, m) (((a % m) - (b % m) + m) % m)
+#define mul_mod(a, b, m) (((a % m) * (b % m)) % m)
+#define gauss(x) ((x*(x+1))/2)
+#define all(vec) vec.begin(), vec.end()
+#define rall(vec) vec.rbegin(), vec.rend()
+#define sz(x) int(x.size())
+#define debug(x) cout << #x << ": " << (x) << "\n";
+#define rep(i, a, b) for(int i = a; i < b; i++)
+#define fi first
+#define se second
+#define pb push_back
+#define eb emplace_back
+#define vi vector<int>
+
+// consts
+const int oo = 4e18;
+const long double pi = acos(-1.0);
+const int mod = (int) 1e9+7;
+const int MAXN = 1e5;
+
+// templates
+template < typename T = int > istream& operator >> (istream &in, vector < T > &v) {
+    for (auto &x : v) in >> x;
+    return in;
+}
+
+template < typename T = int > ostream& operator << (ostream &out, const vector < T > &v) { 
+    for (const T &x : v) out << x << ' '; 
+    return out;
+}
+
+int32_t main ()
+{
+    cin.tie(0)->sync_with_stdio(0);
+
+    int n, k, x;
+    cin >> n >> k >> x;
+
+    vi v(n);
+    cin >> v;
+
+    sort(all(v));
+
+    int ans = 0;
+    for(int i = k-1; i >= 0; i--)
+    {
+        if(x <= 0) break;
+
+        x -= v[i];
+        ans++;
+    }
+
+    if(x > 0)
+    {
+        cout << "-1\n";
+        return 0;
+    }
+
+    ans += n-k;
+
+    cout << ans << '\n';
+
+    return 0;
+}
