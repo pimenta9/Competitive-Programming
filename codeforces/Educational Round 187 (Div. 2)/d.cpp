586106cf64dc1d256a9e2708451237bb9eb75849
João Vitor <109254917+pimenta9@users.noreply.github.com>
2026-02-25 20:37:35 -0300

    Solved: 4 out of 6

---
+++ codeforces/Educational Round 187 (Div. 2)/d.cpp
@@ -0,0 +1,88 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// defines
+#define pii pair<int, int>
+#define int long long
+#define ceil(n, m) (((n) + (m) - 1) / (m))
+#define add_mod(a, b, m) (((a % m) + (b % m)) % m)
+#define sub_mod(a, b, m) (((a % m) - (b % m) + m) % m)
+#define mul_mod(a, b, m) (((a % m) * (b % m)) % m)
+#define gauss(x) ((x*(x+1))/2)
+#define all(vec) vec.begin(), vec.end()
+#define rall(vec) vec.rbegin(), vec.rend()
+#define sz(x) int32_t(x.size())
+#define debug(x) cout << #x << ": " << (x) << "\n";
+#define rep(i, a, b) for(int i = a; i < b; i++)
+#define fi first
+#define se second
+#define pb push_back
+#define eb emplace_back
+#define vi vector<int>
+#define vvi vector<vi>
+
+// consts
+const int oo = 4e18;
+const long double pi = acos(-1.0);
+const int mod = (int) 1e9+7;
+const int MAXN = 1e5;
+
+// templates
+template < typename T = int > istream& operator >> (istream &in, vector < T > &v) {
+    for (auto &x : v) in >> x;
+    return in;
+}
+
+template < typename T = int > ostream& operator << (ostream &out, const vector < T > &v) { 
+    for (const T &x : v) out << x << ' '; 
+    return out;
+}
+//
+
+void solve ()
+{
+    int n, m;
+    cin >> n >> m;
+
+    vi a(n), b(m);
+    cin >> a >> b;
+
+    sort(all(a));
+    a.erase(unique(all(a)), a.end());
+
+    const int max_elem = n + m;
+    vi num_divs(max_elem + 1);
+    for(int i : a)
+    {
+        for(int j = i; j <= max_elem; j += i)
+        {
+            num_divs[j]++;
+        }
+    }
+
+    int A = 0, B = 0, AB = 0;
+    for(int x : b)
+    {
+        if(num_divs[x] == a.size())
+            A++;
+        else if (num_divs[x] == 0)
+            B++;
+        else
+            AB++;
+    }
+
+    if(AB % 2 == 0)
+        cout << (A > B ? "Alice\n" : "Bob\n");
+    else
+        cout << (A >= B ? "Alice\n" : "Bob\n");
+}
+
+int32_t main ()
+{
+    cin.tie(0)->sync_with_stdio(0);
+
+    int t; cin >> t;
+    while(t--) solve();
+
+    return 0;
+}
