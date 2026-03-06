2116ab5d3d2af4acdc7b4fb3dd05de6cf5dece95
João Vitor <109254917+pimenta9@users.noreply.github.com>
2026-03-05 22:50:05 -0300

    Add solution for AtCoder Beginner Contest 446
    Implement solution for AtCoder Beginner Contest 446.

---
+++ atCoder/AtCoder Beginner Contest 446/c.cpp
@@ -0,0 +1,92 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// defines
+#define pii pair<int, int>
+#define int long long
+#define ceil(n, m) (((n) + (m) - 1) / (m))
+#define add_mod(a, b, m) (((a % m) + (b % m)) % m)
+#define sub_mod(a, b, m) (((a % m) - (b % m) + m) % m)
+#define mul_mod(a, b, m) (((a % m) * (b % m)) % m)
+#define gauss(x) ((x*(x+1))/2)
+#define all(vec) vec.begin(), vec.end()
+#define rall(vec) vec.rbegin(), vec.rend()
+#define sz(x) int32_t(x.size())
+#define debug(x) cout << #x << ": " << (x) << "\n";
+#define rep(i, a, b) for(int i = a; i < b; i++)
+#define repr(i, a, b) for(int i = a; i >= b; i--)
+#define fi first
+#define se second
+#define pb push_back
+#define eb emplace_back
+#define vi vector<int>
+#define vvi vector<vi>
+#define endl '\n'
+
+// consts
+// const int oo = 4e18;
+// const long double pi = acos(-1.0);
+// const int mod = (int) 1e9+7;
+// const int MAXN = 1e5;
+// mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+// templates
+template < typename T = int > istream& operator >> (istream &in, vector < T > &v) {
+    for (auto &x : v) in >> x;
+    return in;
+}
+
+template < typename T = int > ostream& operator << (ostream &out, const vector < T > &v) { 
+    for (const T &x : v) out << x << ' '; 
+    return out;
+}
+
+void solve ()
+{
+    int n, d;
+    cin >> n >> d;
+
+    vi a(n), b(n);
+    cin >> a >> b;
+
+    deque<pii> dq;
+    for(int i = 1; i <= n; i++)
+    {
+        int buy = a[i-1];
+        int use = b[i-1];
+
+        dq.eb(buy, i);
+        while(!dq.empty() and use >= dq.front().first)
+        {
+            use -= dq.front().first;
+            dq.pop_front();
+        }
+        if(!dq.empty())
+        {
+            auto [quant, day] = dq.front();
+            dq.pop_front();
+            dq.emplace_front(quant - use, day);
+        }
+
+        if(!dq.empty() and i == dq.front().second + d)
+            dq.pop_front();
+    }
+
+    int ans = 0;
+    while(!dq.empty())
+    {
+        ans += dq.front().first;
+        dq.pop_front();
+    }
+    cout << ans << endl;
+}
+
+int32_t main ()
+{
+    cin.tie(0)->sync_with_stdio(0);
+
+    int t; cin >> t;
+    while(t--) solve();
+
+    return 0;
+}
