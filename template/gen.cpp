a48b3f05af042338920b4649cd057a435a666adf
João Vitor <109254917+pimenta9@users.noreply.github.com>
2026-05-24 18:36:53 -0300

    Create gen.cpp

---
+++ template/gen.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int rand(int a, int b)
+{
+    return a + rand() % (b - a + 1);
+}
+
+int main (int argc, char* argv[])
+{
+    srand(atoi(argv[1]));
+
+    
+
+    return 0;
+}
